246072fd8bb9a115c667685f3461f4b7d0d7cc12
ken-sasaki-222 <nkekisasa222@gmail.com>
2020-08-16 16:17:34 +0900

    update master
    basic認証に必要なコードの記述。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+
+  # basic認証に必要な記述
+  before_action :basic_auth, if: :production?
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Rails.application.credentials[:basic_auth][:user] &&
+      password == Rails.application.credentials[:basic_auth][:pass]
+    end
+  end
+
+  # if: :production?を追加
+  before_action :basic_auth, if: :production?
+
+  def production?
+    Rails.env.production?
+  end
+  
 end
